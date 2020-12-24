f7655c11e53a2d500ed6e19c97efbc18244b68d0
simran1901 <simranmakhijani55@gmail.com>
2020-12-24 16:50:34 +0530

    image input

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,25 @@ import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
-          child: MaterialApp(
+      child: MaterialApp(
         title: 'Great Places',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           accentColor: Colors.amber,
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
     );
   }
