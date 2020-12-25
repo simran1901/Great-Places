a577cf391b488f509122b623d7f609e1b4046c86
simran1901 <simranmakhijani55@gmail.com>
2020-12-25 22:45:43 +0530

    storing image on filesystem

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -12,10 +14,16 @@ class _ImageInputState extends State<ImageInput> {
   File _storedImage;
 
   Future<void> _takePicture() async {
-    final imageFile = await ImagePicker().getImage(
+    final imageFile = File((await ImagePicker().getImage(
       source: ImageSource.camera,
       maxWidth: 600,
-    );
+    )).path);
+    setState(() {
+      _storedImage = imageFile;
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
   }
 
   @override
