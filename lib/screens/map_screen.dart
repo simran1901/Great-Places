5f71fea976197206c59bef709722d3d8e09e6649
simran1901 <simranmakhijani55@gmail.com>
2021-01-06 00:47:59 +0530

    Pick a location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -7,21 +7,41 @@ class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
   final bool isSelecting;
 
-  MapScreen(
-      {this.initialLocation =
-          const PlaceLocation(latitude: 37.422, longitude: -122.084),
-      this.isSelecting = false});
+  MapScreen({
+    this.initialLocation =
+        const PlaceLocation(latitude: 37.422, longitude: -122.084),
+    this.isSelecting = false,
+  });
 
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Map'),
+        actions: <Widget>[
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                    },
+            ),
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -31,6 +51,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? null
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _pickedLocation,
+                ),
+              },
       ),
     );
   }
